81674098fcdc97347e73c0eafdc12536d062afc1
sarfraz <sarfraz_g2009@yahoo.com>
2020-01-04 16:56:00 +0500

    Commit Done

---
+++ aarsol_hr_demo_data/__manifest__.py
@@ -0,0 +1,20 @@
+{
+	'name': 'AARSOL HR Demo Data',
+	'version': '1.0',
+	'category': 'Human Resources/Employees',
+	'sequence': 6,
+	'license': 'AGPL-3',
+	'description': "This module adds the Demo Data for HR.",
+	'author': 'Farooq',
+	'website': 'http://www.aarsolerp.com/',
+	'depends': ['base','hr','hr_recruitment'],
+	'data': [
+		'data/hr_demo.xml',
+	],
+	'license': 'AGPL-3',
+	'installable': True,
+	'auto_install': False,
+	'application': True,
+}
+
+
